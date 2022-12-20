55b8002d6e5f0bd1d8916c8cf19820fe08bc09f3
iZucken <izucken@gmail.com>
2022-12-20 17:09:12 +0300

    refactor: removed datahub based import
    its approach is not effective for really big data imports and it cannot properly ingest large data sources anyway; it is also not as flexible for reporting integration

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,5 @@
 <?php
 
 return [
-    "Pimcore\\Bundle\\DataHubBundle\\PimcoreDataHubBundle" => ['all' => true],
-    "Pimcore\\Bundle\\DataImporterBundle\\PimcoreDataImporterBundle" => ['all' => true],
     "Wvision\\Bundle\\DataDefinitionsBundle\\DataDefinitionsBundle" => ['all' => true],
 ];
