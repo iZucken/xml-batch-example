eeaf039421fb8e86441d96782c578983f98147cc
iZucken <izucken@gmail.com>
2022-12-22 16:57:46 +0300

    chore: added extra parameters to xml counter tool
    to estimate required size for data fields

--- tools/xmlAnalyzer.php
+++ tools/xmlAnalyzer.php
@@ -5,10 +5,16 @@ $reader->open($argv[1]);
 $tags = [];
 $longestContent = 0;
 while ($reader->read()) {
-    $tags[$reader->name] = ($tags[$reader->name] ?? 0) + 1;
-    $longestContent = max(strlen($reader->value), $longestContent);
+    if ($reader->nodeType !== \XMLReader::ELEMENT) {
+        continue;
+    }
+    $tags[$reader->name] = ($tags[$reader->name] ?? [
+        'occurrence' => 0,
+        'maxSize' => 0,
+        'empty' => 0,
+    ]);
+    $tags[$reader->name]['occurrence']++;
+    $tags[$reader->name]['maxSize'] = max(strlen($reader->readOuterXml()), $tags[$reader->name]['maxSize']);
+    $tags[$reader->name]['empty'] += empty($reader->value);
 }
-var_dump([
-    'tags' => $tags,
-    'longestContent' => $longestContent,
-]);
+var_dump($tags);
