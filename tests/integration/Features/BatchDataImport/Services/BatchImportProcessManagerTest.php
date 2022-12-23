d6a53fe10b2de9f2f77c9c2dae9a3482c148feaa
iZucken <izucken@gmail.com>
2022-12-23 10:09:58 +0300

    test: added data cleanup after batch ops

--- tests/integration/Features/BatchDataImport/Services/BatchImportProcessManagerTest.php
+++ tests/integration/Features/BatchDataImport/Services/BatchImportProcessManagerTest.php
@@ -3,14 +3,26 @@
 namespace integration\Features\BatchDataImport\Services;
 
 use App\Features\BatchDataImport\Services\BatchImportProcessManager;
-use PHPUnit\Framework\TestCase;
+use Codeception\Test\Unit;
+use Doctrine\DBAL\Connection;
+use Pimcore\Tests\Helper\Pimcore;
 use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * @coversDefaultClass \App\Features\BatchDataImport\Services\BatchImportProcessManager
  */
-class BatchImportProcessManagerTest extends TestCase
+class BatchImportProcessManagerTest extends Unit
 {
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        /** @var Pimcore $pimcore */
+        $pimcore = $this->getModule('\Pimcore\Tests\Helper\Pimcore');
+        $db = $pimcore->grabService(Connection::class);
+        $db->executeStatement("TRUNCATE TABLE import_logs");
+        $db->executeStatement("TRUNCATE TABLE products");
+    }
+
     /**
      * @covers ::startImportProcess
      */
